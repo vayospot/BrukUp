72f7c3a97e3b6e0369f6d13497d494b5c2855b63
Vayo <ayomipo185@gmail.com>
2024-03-05 20:05:14 +0100

    Used randomuser.me for matching card data

--- app/(tabs)/discover/GridLayout.jsx
+++ app/(tabs)/discover/GridLayout.jsx
@@ -1,12 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import MasonryList from "@react-native-seoul/masonry-list";
-import DATA from "../../../services/data";
+import fetchUserData from "../../../services/data";
+
+import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useState } from "react";
 
 export default function GridLayout() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function getUserData() {
+      try {
+        const userData = await fetchUserData();
+        setData(userData);
+      } catch (error) {
+        alert("There was an error getting the data. Sorry about that.");
+      }
+    }
+
+    getUserData();
+  }, []);
+
   return (
     <MasonryList
-      data={DATA}
-      renderItem={GridItem}
+      data={data}
+      renderItem={MatchCard}
       numColumns={2}
       keyExtractor={(item) => item.id}
       showsVerticalScrollIndicator={false}
@@ -14,15 +32,34 @@ export default function GridLayout() {
   );
 }
 
-function GridItem({ item }) {
+function MatchCard({ item }) {
   return (
     <View
-      className={`justify-center items-center rounded-xl border-white border m-1`}
+      className={`rounded-xl border-white border m-1 overflow-hidden`}
       style={{
         height: item.height,
       }}
     >
-      <Text className='text-white font-questrial'>{item.title}</Text>
+      <ImageBackground
+        source={{ uri: item.image }}
+        resizeMode='cover'
+        className='w-full h-full relative justify-center items-center'
+      >
+        <LinearGradient
+          colors={["#00000040", "#00000040"]}
+          className='absolute bottom-0 w-full flex-row justify-between px-3 py-2'
+        >
+          <Text className='text-white font-questrial'>{item.title}</Text>
+          <View className='bg-neutral-900 rounded-full px-2 items-center justify-center'>
+            <Text
+              className='text-white font-questrial'
+              style={{ fontSize: 10 }}
+            >
+              79%
+            </Text>
+          </View>
+        </LinearGradient>
+      </ImageBackground>
     </View>
   );
 }
--- services/data.js
+++ services/data.js
@@ -1,74 +1,23 @@
-export default DATA = [
-  {
-    id: 1,
-    color: "#ffcdd2",
-    title: "Item 1",
-    height: 200,
-  },
-  {
-    id: 2,
-    color: "#b2dfdb",
-    title: "Item 2",
-    height: 230,
-  },
-  {
-    id: 3,
-    color: "#ffcc80",
-    title: "Item 3",
-    height: 250,
-  },
-  {
-    id: 4,
-    color: "#f0f4c3",
-    title: "Item 4",
-    height: 230,
-  },
-  {
-    id: 5,
-    color: "#b3e5fc",
-    title: "Item 5",
-    height: 200,
-  },
-  {
-    id: 6,
-    color: "#e6ee9c",
-    title: "Item 6",
-    height: 250,
-  },
-  {
-    id: 7,
-    color: "#ffab91",
-    title: "Item 7",
-    height: 230,
-  },
-  {
-    id: 8,
-    color: "#81c784",
-    title: "Item 8",
-    height: 200,
-  },
-  {
-    id: 9,
-    color: "#ce93d8",
-    title: "Item 9",
-    height: 250,
-  },
-  {
-    id: 10,
-    color: "#fff59d",
-    title: "Item 10",
-    height: 230,
-  },
-  {
-    id: 11,
-    color: "#80cbc4",
-    title: "Item 11",
-    height: 250,
-  },
-  {
-    id: 12,
-    color: "#ffab91",
-    title: "Item 12",
-    height: 200,
-  },
-];
+export default async function fetchUserData() {
+  try {
+    const response = await fetch(
+      "https://randomuser.me/api/?results=12&inc=name,gender,id,picture"
+    );
+    const jsonData = await response.json();
+    const userData = jsonData.results.map((item) => ({
+      id: item.id.value,
+      title: item.name.first,
+      image: item.picture.large,
+      height: getRandomHeight(),
+    }));
+    return userData;
+  } catch (error) {
+    alert("There was an error fetching the data. Sorry about that.");
+  }
+}
+
+function getRandomHeight() {
+  const heights = [200, 230, 250];
+  const randomIndex = Math.floor(Math.random() * heights.length);
+  return heights[randomIndex];
+}
